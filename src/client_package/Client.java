8b26fcc6cfe133e7234d8c7b6beaf6d2edaff747
Vathys <deepdesai00@gmail.com>
2019-07-29 15:24:42 -0500

    Added Thread functionality

--- src/client_package/Client.java
+++ src/client_package/Client.java
@@ -1,47 +1,99 @@
 package client_package;
 
+import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.net.InetAddress;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class Client extends Thread
 {
+     private Socket clientSocket;
+     private String serverName;
+     private int port;
+     private int count = 0;
+     
+     public Client(String serverName, int port)
+     {
+          this.serverName = serverName;
+          this.port = port;
+          
+          System.out.println("Connecting to " + serverName + " on port " + port);
+          try
+          {
+               clientSocket = new Socket(serverName, port);
+               System.out.println("Just connected to " + clientSocket.getRemoteSocketAddress());
 
+               OutputStream outToServer = clientSocket.getOutputStream();
+               DataOutputStream out = new DataOutputStream(outToServer);               
+               out.writeUTF("Hello from " + clientSocket.getLocalSocketAddress() + " \r\n");
+               
+          } catch (Exception e)
+          {
+               e.printStackTrace();
+          }
+          
+          this.start();
+     }
+     
+     @Override
+     public void run()
+     {
+          boolean stop = false;
+          try(BufferedReader cin = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())))
+          {
+               while(!stop)
+               {
+                    char temp;
+                    String msg = "";
+                    while(cin.ready())
+                    {
+                         temp = (char) cin.read();
+                         msg += temp;
+                    }
+                    if(msg.isEmpty())
+                    {
+                         count++;
+                    }
+                    if(!msg.isEmpty())
+                    {
+                         System.out.println(msg);
+                         stop = true;
+                    }
+                    if(count >= 1000)
+                    {
+                         stop = true;
+                    }
+               }
+               if(stop)
+               {
+                    try
+                    {
+                         clientSocket.close();
+                    } catch (IOException e)
+                    {
+                         // TODO Auto-generated catch block
+                         e.printStackTrace();
+                    }
+               }
+          } catch (IOException e)
+          {
+               e.printStackTrace();
+          }
+     }
+     
      public static void main(String[] args)
      {
-    	 String serverName = args[0];
+         String serverName = args[0];
          int port = Integer.parseInt(args[1]);
-         try {
-            System.out.println("Connecting to " + serverName + " on port " + port);
-            Socket client = new Socket(serverName, port);
-          
-            System.out.println("Just connected to " + client.getRemoteSocketAddress());
-            
-            OutputStream outToServer = client.getOutputStream(); 
-
-            DataOutputStream out = new DataOutputStream(outToServer);
          
-
-            
-            out.writeUTF("Hello from " + client.getLocalSocketAddress());
-            
-            InputStream inFromServer = client.getInputStream();
-            
-   
-            DataInputStream in = new DataInputStream(inFromServer);
-           
-            System.out.println("Server says " + in.readUTF());
-            client.close();
-         }
-         catch (IOException e) {
-            e.printStackTrace();
-         }
-
-     
-
+         new Client(serverName, port);
+         
 }
 
 }
